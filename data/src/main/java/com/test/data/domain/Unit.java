d28a7774e5efef6acf7aaa901f32032700d67ab8
chensj <chensj@hyxt.com>
2016-11-11 14:51:16 +0800

    update

--- data/src/main/java/com/test/data/domain/Unit.java
+++ data/src/main/java/com/test/data/domain/Unit.java
@@ -1,15 +1,11 @@
 package com.test.data.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Relationship;
 import org.neo4j.ogm.annotation.typeconversion.DateLong;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 @NodeEntity
 public class Unit {
@@ -20,21 +16,9 @@ public class Unit {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date create;
 
-//    @Relationship(type="BELONG", direction = Relationship.INCOMING)
-//    @JsonIgnore
-//    private Set<User> users = new HashSet<>();
-
 
     public Unit() {
     }
-
-//    public Set<User> getUsers() {
-//        return users;
-//    }
-//
-//    public void setUsers(Set<User> users) {
-//        this.users = users;
-//    }
 
     public Long getId() {
         return id;
